8e6143dd1f07a1b6cd7a532dca85e9a41f9bf3d6
khaccount <winekl@dedi3734.your-server.de>
2023-01-16 09:46:45 +0100

    update form server

--- config/database.js
+++ config/database.js
@@ -2,10 +2,17 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'mysql',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'dedi3734.your-server.de'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'winekl_db1'),
+      user: env('DATABASE_USERNAME', 'winekl_1'),
+      password: env('DATABASE_PASSWORD', 'J2tqdPzSu26ZzDHF'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
