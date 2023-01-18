c96125fed515d85be7cb0af7991033264829da65
dev-prospekt <dev@prospekt.hr>
2023-01-18 14:47:59 +0100

    global variable

--- config/database.js
+++ config/database.js
@@ -13,6 +13,16 @@ module.exports = ({ env }) => ({
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
     },
+    // connection: {
+    //   host: env('DATABASE_HOST', '127.0.0.1'),
+    //   port: env.int('DATABASE_PORT', 3306),
+    //   database: env('DATABASE_NAME', 'winekl_db1'),
+    //   user: env('DATABASE_USERNAME', 'root'),
+    //   password: env('DATABASE_PASSWORD', ''),
+    //   // ssl: {
+    //   //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+    //   // },
+    // },
     debug: false,
   },
 });
